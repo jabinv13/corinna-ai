c3e2aa2a21e9b2b70201aadc17c379491e3d2fe8
jabinv13 <jabinv3@gmail.com>
2024-11-20 21:34:13 +0530

    add somehin

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wordpress-1341980-4921589.cloudwaysapps.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
